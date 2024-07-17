97be651dc2fc913fcd14c4e4f591227ff9aeb3b9
Fariza Zea <farizazea9c>
2024-07-17 14:02:20 +0700

    config mini project

---
+++ js/script.js
@@ -0,0 +1,24 @@
+alert("hai")
+// 
+var messageSubmitBMI = function(){
+  console.log("submit-BMI")}
+
+var messageReset = function(){
+    console.log("reset")
+   }
+
+   var messageKonsultasiAhliGiziViaWhatsapp = function(){
+    console.log("konsultasi-ahli-gizi-via-whatsapp")
+   }
+
+   var messageRegistrasiOnlineAhliGizi = function(){
+    console.log("registrasi-online-ahli-gizi")
+   }
+
+var messageKonsultasiDokterViaAplikasi = function(){
+  console.log("konsultasi-dokter-via-aplikasi")
+   }
+
+var messageRegistrasiOnlineSekarang = function(){
+  console.log("registrasi-online-sekarang")
+   }
